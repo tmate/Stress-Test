nodejs/node

commit 83887f35fa0dd71279410cb613c590c02822e29c

repl: support hidden history file on Windows
On Windows when REPL history file has the hidden attribute node will
fail when trying to open it in 'w' mode. This changes the mode to
'r+'. The file is guaranteed to exists because of earlier open call
with 'a+'.

lib/internal/repl.js
@@ -164,10 +164,19 @@ function setupHistory(repl, historyPath, oldHistoryPath, ready) {
       }
     }
 
-    fs.open(historyPath, 'w', onhandle);
+    fs.open(historyPath, 'r+', onhandle);
   }
 
   function onhandle(err, hnd) {
+    if (err) {
+      return ready(err);
+    }
+    fs.ftruncate(hnd, 0, (err) => {
+      return onftruncate(err, hnd);
+    });
+  }
+
+  function onftruncate(err, hnd) {
     if (err) {
       return ready(err);
     }

test/parallel/test-repl-persistent-history.js
@@ -76,6 +76,8 @@ const oldHistoryPath = path.join(fixtures, 'old-repl-history-file.json');
 const enoentHistoryPath = path.join(fixtures, 'enoent-repl-history-file.json');
 const emptyHistoryPath = path.join(fixtures, '.empty-repl-history-file');
 const defaultHistoryPath = path.join(common.tmpDir, '.node_repl_history');
+const emptyHiddenHistoryPath = path.join(fixtures,
+                                         '.empty-hidden-repl-history-file');
 
 const tests = [
   {
@@ -162,6 +164,19 @@ const tests = [
            NODE_REPL_HISTORY_SIZE: 1 },
     test: [UP],
     expected: [prompt, replFailedRead, prompt, replDisabled, prompt]
+  },
+  {
+    before: function before() {
+      if (common.isWindows) {
+        const execSync = require('child_process').execSync;
+        execSync(`ATTRIB +H "${emptyHiddenHistoryPath}"`, (err) => {
+          assert.ifError(err);
+        });
+      }
+    },
+    env: { NODE_REPL_HISTORY: emptyHiddenHistoryPath },
+    test: [UP],
+    expected: [prompt]
   },
   { // Make sure this is always the last test, since we change os.homedir()
     before: function before() {

tmate/Stress-Test

commit 1521a52576e223049ce774b39303b8e21de11324

repl: fix /dev/null history file regression
This fixes a regression from 83887f35fa where ftruncate() fails on
a file symlinked to /dev/null.

PR-URL: https://github.com/nodejs/node/pull/12762
Reviewed-By: Anna Henningsen <anna@addaleax.net>
Reviewed-By: Jeremiah Senkpiel <fishrock123@rocketmail.com>
Reviewed-By: Colin Ihrig <cjihrig@gmail.com>
Reviewed-By: Bartosz Sosnowski <bartosz@janeasystems.com>
Reviewed-By: James M Snell <jasnell@gmail.com>

lib/internal/repl.js
@@ -172,23 +172,16 @@ function setupHistory(repl, historyPath, oldHistoryPath, ready) {
       return ready(err);
     }
     fs.ftruncate(hnd, 0, (err) => {
-      return onftruncate(err, hnd);
-    });
-  }
+      repl._historyHandle = hnd;
+      repl.on('line', online);
 
-  function onftruncate(err, hnd) {
-    if (err) {
-      return ready(err);
-    }
-    repl._historyHandle = hnd;
-    repl.on('line', online);
-
-    // reading the file data out erases it
-    repl.once('flushHistory', function() {
-      repl.resume();
-      ready(null, repl);
+      // reading the file data out erases it
+      repl.once('flushHistory', function() {
+        repl.resume();
+        ready(null, repl);
+      });
+      flushHistory();
     });
-    flushHistory();
   }
 
   // ------ history listeners ------

test/parallel/test-repl-persistent-history.js
@@ -78,6 +78,8 @@ const emptyHistoryPath = path.join(fixtures, '.empty-repl-history-file');
 const defaultHistoryPath = path.join(common.tmpDir, '.node_repl_history');
 const emptyHiddenHistoryPath = path.join(fixtures,
                                          '.empty-hidden-repl-history-file');
+const devNullHistoryPath = path.join(common.tmpDir,
+                                     '.dev-null-repl-history-file');
 
 const tests = [
   {
@@ -175,6 +177,15 @@ const tests = [
       }
     },
     env: { NODE_REPL_HISTORY: emptyHiddenHistoryPath },
+    test: [UP],
+    expected: [prompt]
+  },
+  {
+    before: function before() {
+      if (!common.isWindows)
+        fs.symlinkSync('/dev/null', devNullHistoryPath);
+    },
+    env: { NODE_REPL_HISTORY: devNullHistoryPath },
     test: [UP],
     expected: [prompt]
   },